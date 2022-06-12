6a7a0ac11806f97b8a01e66cb82cbb24e16444c2
Ikaroyo <emmanuel1631@gmail.com>
2022-06-12 18:00:37 -0300

    Update 12/06/2022 16:42

--- js/script_galeria.js
+++ js/script_galeria.js
@@ -1,4 +1,8 @@
 window.onload = loadImages;
+window.setInterval(modificarBackground, 1);
+var grados = 90.0;
+var per1 = 0.0;
+var flagPer1 = 1;
 
 function loadImages() {
     const path = "img/jaspe/galeria/";
@@ -12,4 +16,16 @@ function loadImages() {
         img.alt = "Imagen " + i;
         document.getElementById("galeria").appendChild(img);
     }
+}
+
+function modificarBackground() {
+
+    document.body.style.backgroundImage = 'linear-gradient(' + grados + 'deg, rgb(26, 26, 29) ' + per1 + '%, rgb(111, 34, 50) '+(200-per1)+'%)';
+    grados += 0.2;
+    if (per1 >= 50 && flagPer1 === 1) {
+        flagPer1 = -1;
+    }else if(per1 <= -100 && flagPer1 === -1){
+        flagPer1 = 1;
+    }
+    per1+=flagPer1;
 }
