df203cf806f0c2b5134af88034d1ab8dea0104cb
Ikaroyo <emmanuel1631@gmail.com>
2022-06-12 18:12:08 -0300

    Update 12/06/2022 18:12

--- js/script_catalogo.js
+++ js/script_catalogo.js
@@ -2,16 +2,14 @@ var contador = 0;
 
 
 function addCart() {
-    document.getElementById("items-carrito").innerHTML=contador+1;
+    document.getElementById("items-carrito").innerHTML=(contador+1).toString();
     contador++;
 }
 function removeCart() {
     if (contador === 0) {
-
     alert("No hay nada en el carrito");
     }else{
-        document.getElementById("items-carrito").innerHTML=contador-1;
+        document.getElementById("items-carrito").innerHTML=(contador-1).toString();
         contador--;
     }
-
 }
